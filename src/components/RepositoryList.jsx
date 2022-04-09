562ee1b3fdbe1ad9b72bdb191f87379e1dcc4484
BrHCastro <hendecastro@gmail.com>
2022-04-09 20:55:15 -0300

    Propriedades no React

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,30 +1,14 @@
+import { RepositoryItem } from "./RepositoryItem";
+
 export function RepositoryList() {
   return (
     <section className="repositoryList">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>unform</strong>
-
-          <p>Forms in React</p>
-
-          <a href="#">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-
-          <p>Forms in React</p>
-
-          <a href="#">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-
-          <p>Forms in React</p>
-
-          <a href="#">Acessar repositório</a>
-        </li>
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
       </ul>
     </section>
   );
