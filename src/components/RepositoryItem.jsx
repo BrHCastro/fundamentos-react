232720c4307d4090a2f8afbb276d21ed4b4d36f5
BrHCastro <hendecastro@gmail.com>
2022-04-09 22:05:58 -0300

    HTTP calls

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,11 @@
-export function RepositoryItem() {
+export function RepositoryItem({ repository }) {
   return (
     <li>
-      <strong>unform</strong>
-      <p>Forms in React</p>
-      <a href="#">Acessar repositório</a>
+      <strong>{repository.name}</strong>
+      <p>{repository.description ?? repository.full_name}</p>
+      <a href={repository.html_url} target="_blank">
+        Acessar repositório &rarr;
+      </a>
     </li>
   );
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,14 +1,25 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
+import "../styles/repositories.scss";
+
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/BrHCastro/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repositoryList">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem />
-        <RepositoryItem />
-        <RepositoryItem />
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.id} repository={repository} />
+        ))}
       </ul>
     </section>
   );
