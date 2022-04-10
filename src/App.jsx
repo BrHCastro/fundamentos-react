ca668cfc6b1b933a114bbd8768f151ae153235ab
BrHCastro <hendecastro@gmail.com>
2022-04-09 21:24:23 -0300

    Fast Refresh in Webpack

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Counter } from "./components/Couter";
 import { RepositoryList } from "./components/RepositoryList";
 
 export function App() {
-  return <RepositoryList />;
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  );
 }
---
+++ src/components/Couter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function handleIncrement() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <>
+      <h2>{counter}</h2>
+      <button onClick={handleIncrement} type="button">
+        Increment +1
+      </button>
+    </>
+  );
+}
